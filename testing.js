e50b8607bc614a80f44733492939056946613f26
Alex Seeley <alexseeley134@gmail.com>
2024-10-03 14:18:02 -0600

    Remove sum and add testing basics

---
+++ testing.js
@@ -0,0 +1,5 @@
+function capitalize(prop) {
+  return prop.charAt(0).toUpperCase() + prop.slice(1);
+}
+
+module.exports = capitalize;
---
+++ testing.test.js
@@ -0,0 +1,7 @@
+// A capitalize function that takes a string and returns it with the first character capitalized.
+
+const capitalize = require("./testing.js");
+
+test("files are testing well", () => {
+  expect(capitalize("string")).toBe("String");
+});
